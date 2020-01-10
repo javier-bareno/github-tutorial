01bf2d6a75bd3f7b4d3f49cfe7927b53af7931f7
javier-bareno <javier.bareno@gmail.com>
2020-01-10 13:23:53 -0600

    Exercise 1 initial commit

---
+++ exercise1/exercise1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 13:18:21 2020
+
+@author: javierbareno
+"""
+
+def print_message():
+    print("Printed a message")
+    return
+
+if __name__ == "__main__":
+    print_message()
+    
